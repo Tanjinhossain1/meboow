292bcc304de74dc1fe6cd2ac25820dcd7fc8f0aa
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2024-07-18 01:00:23 +0600

    add canontial in new detail page

--- src/app/details/[id]/[category]/[title]/page.tsx
+++ src/app/details/[id]/[category]/[title]/page.tsx
@@ -1,10 +1,12 @@
 import DetailsComponent from "@/Component/Details/Details";
 import Navbar from "@/Component/Shared/Navbar";
-import { fetchArticles, fetchArticlesDetails, fetchCategories } from "@/services/articleServices";
+import {
+  fetchArticles,
+  fetchArticlesDetails,
+  fetchCategories,
+} from "@/services/articleServices";
 import { Metadata, ResolvingMetadata } from "next";
 import React from "react";
-
-
 
 export async function generateMetadata(
   { params }: { params: { id: string } },
@@ -12,7 +14,7 @@ export async function generateMetadata(
 ): Promise<Metadata> {
   const articleDetail = await fetchArticlesDetails({ id: params?.id });
   const title = articleDetail?.data[0]?.title;
-  const desc = articleDetail?.data[0]?.description.slice(0,130)
+  const desc = articleDetail?.data[0]?.description.slice(0, 130);
   const previousImages = (await parent).openGraph?.images || [];
   const image = articleDetail?.data[0].image;
 
@@ -26,29 +28,52 @@ export async function generateMetadata(
   };
 }
 
-interface DetailsParams{
+interface DetailsParams {
   searchParams: {
     page: string;
     limit: string;
-    category:string
+    category: string;
   };
   params: {
     id: string;
     category: string;
+    title: string;
   };
 }
-export default async function Details({ params,searchParams }: DetailsParams) {
+export default async function Details({ params, searchParams }: DetailsParams) {
   const data = await fetchArticlesDetails({ id: params?.id });
   const Category = await fetchCategories();
-  const articles = await fetchArticles({category: params?.category,page:searchParams?.page,limit:searchParams?.limit});
+  const articles = await fetchArticles({
+    category: params?.category,
+    page: searchParams?.page,
+    limit: searchParams?.limit,
+  });
+  const decodedTitle = decodeURIComponent(params?.title);
+
+  const formattedTitle = decodedTitle
+    .split("-")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+
+    const joinTitle = formattedTitle
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join("-");
   return (
     <>
-    
-    <Navbar />
-    {
-      data?.data ? 
-      <DetailsComponent articles={articles.data} category={Category.data} articleDetail={data?.data[0]} />
-    : null}
+      <link
+        rel="canonical"
+        href={`${process.env.NEXT_APP_URL}/details/${params.id}/${params.category}/${joinTitle}`}
+        key="canonical"
+      />
+      <Navbar />
+      {data?.data ? (
+        <DetailsComponent
+          articles={articles.data}
+          category={Category.data}
+          articleDetail={data?.data[0]}
+        />
+      ) : null}
     </>
   );
 }
