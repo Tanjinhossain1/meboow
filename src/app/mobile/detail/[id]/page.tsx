18b9f1ac1e03d536feca1f887122741443b65fdb
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2024-07-17 21:00:48 +0600

    fix old

--- src/app/mobile/detail/[id]/page.tsx
+++ src/app/mobile/detail/[id]/page.tsx
@@ -5,11 +5,9 @@ import { fetchMobileArticleDetails } from "@/services/articleServices";
 import { Metadata, ResolvingMetadata } from "next";
 import dynamic from "next/dynamic";
 
-const TopMobileDetails = dynamic(() => import("./_components/TopDetail"),
-{ ssr: false });
+const TopMobileDetails = dynamic(() => import("./_components/TopDetail"));
 const BottomMobileDetails = dynamic(
-  () => import("./_components/BottomDetails"),
-  { ssr: false }
+  () => import("./_components/BottomDetails")
 );
 
 export async function generateMetadata(
@@ -21,8 +19,8 @@ export async function generateMetadata(
   const desc = `Here will show this ${mobileArticles?.data[0]?.title} mobile details and specifications this mobile is this ${mobileArticles?.data[0]?.brands} brand. you can see all of details of this phone.`;
   const previousImages = (await parent).openGraph?.images || [];
   const image = mobileArticles?.data[0]?.display_image;
- 
-  return{
+
+  return {
     title: title,
     description: desc,
 
@@ -30,26 +28,21 @@ export async function generateMetadata(
       images: [image, ...previousImages],
     },
   };
- 
 }
 
 const ProductDetails = async ({ params }: { params: { id: string } }) => {
   const mobileArticles = await fetchMobileArticleDetails({ id: params?.id });
 
   return (
-    <Fragment> 
-      <Suspense>
-        <Navbar />
-      </Suspense>
+    <Fragment>
+      <Navbar />
       {mobileArticles.data[0] ? (
         <TopMobileDetails mobileArticles={mobileArticles.data[0]} />
       ) : null}
       {mobileArticles.data[0] ? (
         <BottomMobileDetails mobileArticles={mobileArticles.data[0]} />
       ) : null}
-      <Suspense>
-        <Footer />
-      </Suspense>
+      <Footer />
     </Fragment>
   );
 };
