0435598535b351a8916c8fbbde0cf7305923c77e
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2024-07-17 23:50:17 +0600

    add lazy loading

--- src/app/mobile/detail/[id]/page.tsx
+++ src/app/mobile/detail/[id]/page.tsx
@@ -5,11 +5,13 @@ import { fetchMobileArticleDetails } from "@/services/articleServices";
 import { Metadata, ResolvingMetadata } from "next";
 import dynamic from "next/dynamic";
 
-const TopMobileDetails = dynamic(() => import("./_components/TopDetail"));
-const BottomMobileDetails = dynamic(
-  () => import("./_components/BottomDetails")
-);
+// const TopMobileDetails = dynamic(() => import("./_components/TopDetail"));
+// const BottomMobileDetails = dynamic(
+//   () => import("./_components/BottomDetails")
+// );
 
+const TopMobileDetails = React.lazy(() => import("./_components/TopDetail"));
+const BottomMobileDetails = React.lazy(() => import("./_components/BottomDetails"));
 export async function generateMetadata(
   { params }: { params: { id: string } },
   parent: ResolvingMetadata
