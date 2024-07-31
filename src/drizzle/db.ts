926bb9afa04b2c74995075949d296f3b5d39416a
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2024-07-31 23:05:17 +0600

    add connect

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -6,13 +6,9 @@ import * as schema from './schema';
 let database: ReturnType<typeof drizzle> | null = null;
 
 export async function getDb() {
-  try {
-    if (!database) {
-      const connection = await mysql.createConnection(process.env.DATABASE_URL!);
-      database = drizzle(connection, { schema, mode: "default" });
-      return database;
-    }
-  } catch (error) {
-    console.log('database connection error ', error)
+  if (!database) {
+    const connection = await mysql.createConnection(process.env.DATABASE_URL!);
+    database = drizzle(connection, { schema,mode: "default"  });
   }
+  return database;
 }
