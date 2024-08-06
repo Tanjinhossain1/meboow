4a837d7c9ebb3b18398bf7e0f55be1daf1e419bb
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2024-08-06 18:07:06 +0600

    try to fix

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from 'next'
  
-export default async function sitemap({res}:any): Promise<MetadataRoute.Sitemap> {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   return [
     {
       url: `${process.env.NEXT_APP_SITEMAP_URL}/aboutus`,
