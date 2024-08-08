aeb543333a776e040ae1519b504e580c1050feaf
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2024-08-08 18:41:46 +0600

    add sitemap in pages

--- next-sitemap.js
+++
@@ -1,6 +0,0 @@
-// next-sitemap.js
-module.exports = {
-    siteUrl: process.env.NEXT_APP_SITEMAP_URL || 'https://example.com',
-    generateRobotsTxt: true, // (optional) Generate robots.txt file\
-  };
-  
