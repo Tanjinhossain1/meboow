12fe1e0dbb00a63a097f43b5fe5b4f417eec2b90
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2024-08-14 16:34:56 +0600

    add about page

---
+++ src/app/aboutus/page.tsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import Navbar from "@/Component/Shared/Navbar";
+import AboutUsComponent from "./_components/AboutUsComponent";
+import Footer from "@/Component/HomePage/Footer";
+
+export default async function AboutUs() {
+  return (
+    <Fragment>
+      <Navbar />
+      <AboutUsComponent />
+      {/* <Footer /> */}
+    </Fragment>
+  );
+}
