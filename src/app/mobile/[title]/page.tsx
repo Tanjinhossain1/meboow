803df2973c4ba5a106c1aaced916ecee92badc58
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2024-09-14 21:47:35 +0600

    add heigt and width in image

--- src/app/mobile/[title]/page.tsx
+++ src/app/mobile/[title]/page.tsx
@@ -52,8 +52,8 @@ export async function generateMetadata(
           {
             url: `${process.env.NEXT_PUBLIC_IMAGE_SERVER_URL}/get/${image}`,
             alt: `${title} image`,
-            width: 800, // optional, adjust based on your image size
-            height: 600, // optional, adjust based on your image size
+            width: 400, // optional, adjust based on your image size
+            height: 300, // optional, adjust based on your image size
           },
           ...previousImages,
         ],
