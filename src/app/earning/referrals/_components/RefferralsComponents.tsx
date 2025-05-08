e487ea4078f30267bf21b6da07d275fb799ff826
Tanjinhossain1 <tanjinhossain2003@gmail.com>
2025-05-08 18:31:05 +0600

    complete the referral frontend and backend both

--- src/app/earning/referrals/_components/RefferralsComponents.tsx
+++ src/app/earning/referrals/_components/RefferralsComponents.tsx
@@ -71,8 +71,6 @@ export default function ReferralProgram({user, referralData}:{user:any, referral
         <Copy className="w-5 h-5 mr-2" />
         {copied ? "Copied!" : "Copy Referral Link"}
       </button>
-
-      <div className="text-center mt-8 text-gray-500 text-sm">© 2025 RupeeTub</div>
     </div>
   )
 }
